ef33c98c9f8e9f3aa4de5b416a3a8b6cee9ddbbe
Loris Polesana <zpolel@bbcag.ch>
2024-06-20 10:42:04 +0200

    Init PrimeReact

---
+++ Flights/pages/_app.jsx
@@ -0,0 +1,19 @@
+import {PrimeReactProvider, PrimeReactContext} from "primereact/api";
+import 'primereact/resources/themes/lara-dark-cyan/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+export default function App({Component, pageProps}) {
+    return (
+        <PrimeReactProvider>
+            <div>
+                <div>
+                    <main>
+                        <Component {...pageProps} />
+                    </main>
+                </div>
+            </div>
+        </PrimeReactProvider>
+    )
+}
