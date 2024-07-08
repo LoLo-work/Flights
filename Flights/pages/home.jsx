751cca0bcda59915f17dc340a22f691abe58990d
Roman Berli <roman.berli2007@gmail.com>
2024-07-08 15:21:14 +0200

    styling

--- Flights/pages/home.jsx
+++ Flights/pages/home.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import SearchFlight from 'components/FlightsTable';
+import React from "react";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import SearchFlight from "components/FlightsTable";
 
 export default function HomePage() {
-
-
-
   return (
     <div>
-        <Header></Header>
+      <Header></Header>
+      <div className="m-7 text-center">
         <br />
         <br />
+        <h2>Alle Flüge</h2>
+        <p>Hier sehen Sie alle Flüge, welche wir als <i>Flights©</i> anbieten</p>
+        </div>
+
         <SearchFlight></SearchFlight>
+        <div className="m-7 text-center">
+
         <br />
         <br />
         <br />
-        <Footer></Footer>
+      </div>
+      <Footer></Footer>
     </div>
   );
-};
+}
